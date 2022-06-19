7b4a810daf8441fa6142573439aacbb6334f92ef
suga-ming <kby9901@naver.com>
2022-06-19 15:42:47 +0900

    git test

--- src/Board.js
+++ src/Board.js
@@ -5,7 +5,7 @@ function Board({ name, color, num, sum, gameHistory }) {
   <div>
     <h2>{name}</h2>
     <Dice color={color} num={num}/>
-    <h2>총점!</h2>
+    <h2>총점</h2>
     <p>{sum}</p>
     <h2>기록</h2>
     <p>{gameHistory.join(', ')}</p>
