081dc5942d66c5c25639b7c7bc9d318bc3bf3bbc
suga-ming <kby9901@naver.com>
2022-06-20 18:17:23 +0900

    타이틀

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from './Button';
 import Board from './Board'
 import './App.css'
+import logo from './assets/logo.png'
 
 function random(n) {
   return Math.ceil(Math.random() * n)
@@ -27,14 +28,18 @@ function App() {
 
   return (
     <div className='App'>
+      <div className='App-logo'>
+        <div className='logo-img'><img src={logo} alt='logo' width='90px' height='55px'></img></div>
+        <div className='App-title'>주사위 게임</div>
+      </div>
       <div>
         <Button className="App-button" color="blue" onClick={handleRollClick}>던지기</Button>
         <Button className="App-button" color="red" onClick={handleClearClick}>처음부터</Button>
       </div>
-      <div>
+      <div className='board'>
         <Board name="나" color="blue"  gameHistory={myHistory} />
         </div>
-        <div>
+        <div className='board'>
         <Board name="상대" color="red"  gameHistory={otherHistory} />
       </div>
     </div>
